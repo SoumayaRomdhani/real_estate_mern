f48ee427370a2e0b8545579014f106586aaca322
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-08-15 14:21:37 +0100

    ad create listing API route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/userroute.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import listingRouter from "./routes/listing.route.js";
 
 dotenv.config();
 
@@ -33,6 +34,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
