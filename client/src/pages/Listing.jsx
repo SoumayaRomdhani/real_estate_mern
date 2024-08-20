265c8e1320d959919b8704a217da8c2e68a7bdc7
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-08-20 02:36:19 +0100

    create the listing item component and show listing

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -27,7 +27,7 @@ export default function Listing() {
 
   const [contact, setContact] = useState(false); 
   const params = useParams();
-  const {currentUser} = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
     const fetchListing = async () => {
       try {
@@ -142,7 +142,7 @@ export default function Listing() {
                 Contact landlord
               </button>
             )}
-            {contact && <Contact listing={listing}/>}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
