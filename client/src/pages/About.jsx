1dc299c632d8d2341bb676c4204f6df7bfb9e262
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-08-23 14:39:53 +0100

    Update About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 export default function About() {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
-      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Sahand Estate</h1>
-      <p className='mb-4 text-slate-700'>Sahand Estate is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
+      <h1 className='text-3xl font-bold mb-4 text-teal-600'>Welcome to StudentNest!</h1>
+      <p className='mb-4 text-slate-700'> Your premier destination for university housing. We make finding your ideal living space a breeze—whether you’re seeking a comfortable dorm, a budget-friendly apartment, or a practical shared flat. Our platform offers a range of tailored listings, with real-time updates and easy-to-use filters, designed specifically for student needs. </p>
       <p className='mb-4 text-slate-700'>
-      Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
+      Explore your options and discover a home that perfectly complements your university lifestyle. Start your search with StudentNest today and find your ideal residence with ease!
       </p>
-      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+      <p className='mb-4 text-slate-700'>Our carefully curated listings ensure you get access to the best student-friendly properties available. Enjoy features like detailed property descriptions, high-quality images, and user reviews to make informed decisions. With StudentNest, finding a place to call home during your studies has never been easier. Join our community of students and start your journey to finding the perfect student accommodation now!
+
+</p>
     </div>
   )
 }
